44eb527f3488b1590ddf080a5418d7ff5ace7b24
markodjj <markodjordjevic244@gmail.com>
2024-09-24 19:56:42 +0200

    Finish components and add css

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,12 +1,53 @@
+import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function persisData(newList){
+    localStorage.setItem('todos', JSON.stringify({todos:newList}))
+  }
+
+  function handleAddTodos(newTodo){
+    const newTodoList = [...todos, newTodo]
+    persisData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index){
+    const newTodoList = todos.filter((todo, todoIndex) =>{
+      return todoIndex !== index
+    })
+    persisData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index){
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(()=>{
+    if(!localStorage){
+      return
+    }
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos){
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
 
   return (
-   <main>
-      <TodoInput />
-      <TodoList />
-   </main>
+   <>
+      <TodoInput handleAddTodos={handleAddTodos} setTodoValue={setTodoValue} todoValue={todoValue} />
+      <TodoList handleEditTodo={handleEditTodo}  handleDeleteTodo={handleDeleteTodo} todos={todos} />
+   </>
   )
 }
 
--- todolist/src/components/TodoCard.jsx
+++ todolist/src/components/TodoCard.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-export default function TodoCard() {
-  return (
-    <div>TodoCard</div>
-  )
+export default function TodoCard(props) {
+    const {children, handleDeleteTodo, index, handleEditTodo} = props
+
+    return (
+        <li className='todoItem'>
+            <div className='actionsContainer'>
+                {children}
+                <button onClick={()=>{
+                    handleEditTodo(index)
+                }}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+
+                <button onClick={()=>{
+                    handleDeleteTodo(index)
+                }}>
+                    <i className="fa-regular fa-trash-can"></i>
+                </button>
+                
+                
+            </div>
+        
+        </li>
+    )
 }
